f1696221c3e15c991d86313ab0236d5e4ad1c384
RaulORios <raul.orios89@outlook.com>
2021-08-10 12:07:34 -0300

    Arquivos atualizados

---
+++ Uri_1035/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uri_1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int c = int.Parse(valores[2]);
+            int d = int.Parse(valores[3]);
+
+
+            if (b > c && d > a && c + d > a + b && d > 0 && c > 0 && a % 2 == 0) 
+            {
+                mensagem = "Valores aceitos";
+            }
+            else
+	        {
+                mensagem = "Valores nao aceitos";
+            }
+
+            Console.WriteLine(mensagem);
+
+        }
+    }
+}
